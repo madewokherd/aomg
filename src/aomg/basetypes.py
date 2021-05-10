39a460a2faf0689f01f702b5d82172b622bf5827
Esme <madewokherd@gmail.com>
2021-05-10 14:53:18 -0500

    Update copyright name.

--- src/aomg/basetypes.py
+++ src/aomg/basetypes.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Vincent Povirk
+# Copyright (c) 2019-2021 Esme Povirk
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
